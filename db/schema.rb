183424a93e6434e8a39a4ca8b3da8ccead0074ce
Benjamin Mosior <bemosior@ship.edu>
2014-02-19 20:53:33 -0500

    Added Report model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140220013547) do
+ActiveRecord::Schema.define(version: 20140220015248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,12 @@ ActiveRecord::Schema.define(version: 20140220013547) do
   end
 
   add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
+
+  create_table "reports", force: true do |t|
+    t.text     "reproduce"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
