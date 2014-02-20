85e638b22ec79c746885705e60fbe0167f6e0c92
Benjamin Mosior <bemosior@ship.edu>
2014-02-19 20:54:59 -0500

    Added Service model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140220015248) do
+ActiveRecord::Schema.define(version: 20140220015421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,12 @@ ActiveRecord::Schema.define(version: 20140220015248) do
 
   create_table "reports", force: true do |t|
     t.text     "reproduce"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "services", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
