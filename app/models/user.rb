23429c741193a2596a15fdc6ba91b03d047af8d5
Benjamin Mosior <bemosior@ship.edu>
2014-02-21 14:53:01 -0500

    Added login attribute to User, update model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :ldap_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :ldap_authenticatable, :rememberable, :trackable
 
   has_many :report
 end
