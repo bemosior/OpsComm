82c52975a5d7851e908fc4b675cfabc862ededfb
Benjamin Mosior <bemosior@ship.edu>
2014-02-21 14:56:09 -0500

    Fixed User login form and model attribute lookups via LDAP

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ActiveRecord::Base
   devise :ldap_authenticatable, :rememberable, :trackable
 
   has_many :report
+
+  def get_ldap_email
+    self.email = Devise::LDAP::Adapter.get_ldap_param(self.login,"mail")
+  end
+
+  def get_ldap_name
+    self.name = Devise::LDAP::Adapter.get_ldap_param(self.login,"name")
+  end
+
 end
