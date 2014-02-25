13f5cb69857bb4f74b01b5ae44b1fd4b424978b7
Benjamin Mosior <bemosior@ship.edu>
2014-02-24 21:43:13 -0500

    Fixed report feedback (v2)

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -12,9 +12,9 @@ class ReportController < ApplicationController
     @report.user = current_user
     if @report.save
       NotificationMailer.notification_email(@report).deliver
-      redirect_to action: 'contact', flash: { notice: "Communication Initiated" }
+      redirect_to action: 'contact', flash: { "Communication Initiated" }
     else
-      redirect_to action: 'contact', flash: { error: "A Failure Occurred" }
+      redirect_to action: 'contact', flash: { "A Failure Occurred" }
     end
   end
 
