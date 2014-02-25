004dfad4b56d8689e44a4db5bffd1c746c2c4e0e
Benjamin Mosior <bemosior@ship.edu>
2014-02-25 10:51:15 -0500

    Disabled rememberable function for User.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :ldap_authenticatable, :rememberable, :trackable
+  devise :ldap_authenticatable, :trackable
 
   has_many :report
 
