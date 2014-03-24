1463625efbe41ee85076c4a5726250ffc0af2518
Benjamin Mosior <bemosior@Benjamin-Mosiors-MacBook.local>
2014-03-23 23:52:58 -0400

    Updated path to include imagemagick bins

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,4 +35,6 @@ OpsComm::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+
+  ENV['PATH'] += File::PATH_SEPARATOR + '/usr/local/bin'
 end
